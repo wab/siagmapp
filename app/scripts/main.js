47ec2a166bbf9f6d6c516a8d11a43dd3a58a2082
wab [odev] <wab2com@gmail.com>
2014-11-19 16:53:55 +0100

    int. 1911

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+console.log('\'Allo \'Allo!');
+$(function() {
+	'use strict';
+    var $menu = $('nav#menuActivites');
+    $menu.mmenu({
+    }, {
+        selectedClass: 'active'
+		
+    });
+});
