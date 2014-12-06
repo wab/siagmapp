5a6d0fa91a7dbdb55d8c54cdd7ff7f6111e463c5
wab <contact@wabdesign.fr>
2014-12-06 02:19:16 +0100

    gulp file build

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var app = './app/';
 
-var gulp = require('gulp'),
-	connect = require('gulp-connect'),
-	mainBowerFiles  = require('main-bower-files'),
-  inject = require('gulp-inject'),
-  less = require('gulp-less'),
-  gulpif = require('gulp-if'),
-  uglify = require('gulp-uglify'),
-  minifyCss = require('gulp-minify-css'),
-  gulpFilter = require('gulp-filter'),
-  useref = require('gulp-useref');
+  var gulp       = require('gulp'),
+  connect        = require('gulp-connect'),
+  del          = require('del'),
+  mainBowerFiles = require('main-bower-files'),
+  inject         = require('gulp-inject'),
+  less           = require('gulp-less'),
+  gulpif         = require('gulp-if'),
+  uglify         = require('gulp-uglify'),
+  minifyCss      = require('gulp-minify-css'),
+  gulpFilter     = require('gulp-filter'),
+  copy           = require('gulp-copy'),
+  htmlmin        = require('gulp-htmlmin'),
+  imagemin       = require('gulp-imagemin'),
+  useref         = require('gulp-useref');
 
 gulp.task('bower', function() {
     return gulp.src(app + 'index.html')
@@ -31,6 +35,10 @@ gulp.task('connect', function() {
             ];
           }
   });
+});
+
+gulp.task('clean', function () {
+    return del('dist');
 });
 
 gulp.task('reload', function () {
@@ -59,9 +67,26 @@ gulp.task('watch', function() {
 
 });
 
+gulp.task('copy', function() {
+  gulp.src(app + '*.*')
+    .pipe(gulp.dest('dist'));
+  gulp.src(app +'fonts/**/*.{ttf,woff,eot,svg}')
+    .pipe(gulp.dest('dist/fonts'));
+   gulp.src(app +'contenus/*.json')
+    .pipe(gulp.dest('dist/contenus'));
+  gulp.src('app/views/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/views'));
+  gulp.src('app/images/**/*')
+    .pipe(imagemin({
+            progressive: true
+        }))
+    .pipe(gulp.dest('dist/images'));
+})
+
 gulp.task('default', ['connect', 'watch']);
 
-gulp.task('build',  function() {
+gulp.task('build', ['clean', 'less', 'copy'],  function() {
   var assets = useref.assets();
 
     return gulp.src(app +'/index.html')
@@ -71,4 +96,5 @@ gulp.task('build',  function() {
        .pipe(assets.restore())
        .pipe(useref())
        .pipe(gulp.dest('dist'));
+
 });
